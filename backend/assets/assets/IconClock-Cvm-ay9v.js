a354f83a2b42817a2befcd08db0fbbbacddabbee
Piotr Boruc <piotr.m.boruc@gmail.com>
2026-02-15 22:12:53 +0100

    chore: exclude frontend build artifacts from git tracking
    - Add backend/assets/ to .gitignore
    - Remove all tracked files in backend/assets/
    - CI/CD will generate these during build process
    - rust-embed will embed assets at compile time from generated files

--- backend/assets/assets/IconClock-Cvm-ay9v.js
+++
@@ -1,1 +0,0 @@
-import{e as o}from"./index-mue86wS4.js";var c=o("clock","IconClock",[["path",{d:"M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0",key:"svg-0"}],["path",{d:"M12 7v5l3 3",key:"svg-1"}]]);export{c as I};
