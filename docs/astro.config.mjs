ea8ff759bea9bcc8447e8bc057c63b81cb91e4c4
Piotr Boruc <piotr.m.boruc@gmail.com>
2026-02-23 21:00:41 +0100

    version bump

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig({
 			plugins: [
 				starlightVersions({
 					versions: [
-					{ slug: '1.2', label: 'v1.2.x' },
+						{ slug: '1.2', label: 'v1.2.x' },
 					],
 					current: {
 						label: 'Latest',
