34726a2360a49c42a4bfad9a13ff95f6aeac65d0
Piotr Boruc <piotr.m.boruc@gmail.com>
2026-02-24 10:02:58 +0100

    cleanup

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -19,8 +19,8 @@ export default defineConfig({
 			plugins: [
 				starlightVersions({
 					versions: [
+						{ slug: '1.2', label: 'v1.2.x' },
 						{ slug: '1.1', label: 'v1.1.x' },
-						{ slug: '0.2', label: 'v0.2.x' },
 					],
 					current: {
 						label: 'Latest (v1.2.x)',
